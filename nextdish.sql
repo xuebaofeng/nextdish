5b394bf0fb21eeae30e07af81aec610cb42bb8d1
Baofeng Xue <xbaofeng@gmail.com>
2021-04-21 09:27:05 -0700

    init

--- nextdish.sql
+++ nextdish.sql
@@ -7,8 +7,11 @@ CREATE TABLE payment (
 	status TEXT NOT NULL,
 	PRIMARY KEY(date, note)
 );
-
---INSERT INTO payment (date, note, amount, status) VALUES('', '', 0, '');
+drop table payment_sms;
+CREATE TABLE payment_sms (
+	amount REAL,
+	date TEXT
+);
 
 --DELETE from payment ;
 
@@ -18,9 +21,9 @@ SELECT sum(amount) from payment p ;
 SELECT sum(amount) from payment p where p.status ='Paid';
 SELECT sum(amount) from payment p where p.status ='Paid' and p.note <> 'Customer Tips';
 
-INSERT INTO payment_sms (amount, date) VALUES(449.5, '2021-04-21');
+INSERT INTO payment_sms (amount, date) VALUES(449.50, '2021-04-21');
 INSERT INTO payment_sms (amount, date) VALUES(361.25, '2021-04-14');
 INSERT INTO payment_sms (amount, date) VALUES(416.81, '2021-04-06');
 INSERT INTO payment_sms (amount, date) VALUES(209.75, '2021-03-30');
 
-select sum(Amount) from payment_sms;
+select sum(amount) from payment_sms;
