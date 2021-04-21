68682c9a8482dae3b835a5c5fe1168c4b920cdcb
Baofeng Xue <xbaofeng@gmail.com>
2021-04-21 09:25:49 -0700

    init

--- nextdish.sql
+++ nextdish.sql
@@ -17,3 +17,10 @@ SELECT * from payment p ;
 SELECT sum(amount) from payment p ;
 SELECT sum(amount) from payment p where p.status ='Paid';
 SELECT sum(amount) from payment p where p.status ='Paid' and p.note <> 'Customer Tips';
+
+INSERT INTO payment_sms (amount, date) VALUES(449.5, '2021-04-21');
+INSERT INTO payment_sms (amount, date) VALUES(361.25, '2021-04-14');
+INSERT INTO payment_sms (amount, date) VALUES(416.81, '2021-04-06');
+INSERT INTO payment_sms (amount, date) VALUES(209.75, '2021-03-30');
+
+select sum(Amount) from payment_sms;
