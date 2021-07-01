f472d30119fff5af1d29df49a3d6f3e9a8429e71
Baofeng Xue <xbaofeng@gmail.com>
2021-06-30 19:36:46 -0700

    r

--- nextdish.sql
+++ nextdish.sql
@@ -1,5 +1,4 @@
 drop table payment;
-
 CREATE TABLE payment (
 	date TEXT NOT NULL,
 	note TEXT NOT NULL,
@@ -7,32 +6,41 @@ CREATE TABLE payment (
 	status TEXT NOT NULL,
 	PRIMARY KEY(date, note)
 );
+
 drop table payment_sms;
 CREATE TABLE payment_sms (
 	amount REAL,
-	date TEXT
+	date TEXT,
+	PRIMARY KEY(date)
 );
 
+drop table payment_last;
 CREATE TABLE payment_last (
-	date TEXT
+	amount REAL,
+	date TEXT,
+	PRIMARY KEY(date)
 );
-
---DELETE from payment ;
 
 SELECT * from payment p where p.date > (select max(date) from payment_last ps)  order by date desc;
-
-SELECT sum(amount) from payment p where p.date > (select max(date) from payment_last ps);
-select sum(amount) from payment_sms p where p.date > (select max(date) from payment_last ps);
+select * from payment_sms p where p.date > (select max(date) from payment_last ps) order by date desc;
 
 SELECT sum(amount) from payment p where p.status ='Paid' and p.date > (select max(date) from payment_last ps);
-SELECT sum(amount) from payment p where p.status ='Paid' and p.note <> 'Customer Tips' and p.date > (select max(date) from payment_last ps);
 
-select * from payment_sms ps  order by date desc;
+
 select * from payment_last ps;
-select * from payment ps order by date desc;
 
-INSERT INTO payment_last (date) VALUES('20210427');
+update payment  set status = 'Paid' where status = '已付款';
 
+select  '总收款:' || (select sum(amount) from payment_sms p where p.date > (select max(date) from payment_last ps) and p.date <= (select max(date) from payment_sms ps)) 
+|| ', 应付保锋:' || sum(amount) from payment p where p.status ='Paid' and p.note <> 'Customer Tips'  and p.date > (select max(date) from payment_last ps)  and p.date <= (select max(date) from payment_sms ps);
+
+
+
+INSERT INTO payment_last (amount, date) VALUES(1376, '20210427');
+
+
+INSERT INTO payment_sms (amount, date) VALUES(360.84, '20210630');
+INSERT INTO payment_sms (amount, date) VALUES(373.41, '20210623');
 INSERT INTO payment_sms (amount, date) VALUES(433.33, '20210616');
 INSERT INTO payment_sms (amount, date) VALUES(173.50, '20210609');
 INSERT INTO payment_sms (amount, date) VALUES(345.33, '20210602');
@@ -46,12 +54,6 @@ INSERT INTO payment_sms (amount, date) VALUES(361.25, '20210414');
 INSERT INTO payment_sms (amount, date) VALUES(416.81, '20210406');
 INSERT INTO payment_sms (amount, date) VALUES(209.75, '20210330');
 
-
-
-update payment  set status = 'Paid' where status = '已付款';
-
-select  '总收款:' || (SELECT sum(amount) from payment p where p.status ='Paid'  and p.date > (select max(date) from payment_last ps)) 
-|| ', 应付保锋:' || sum(amount) from payment p where p.status ='Paid' and p.note <> 'Customer Tips'  and p.date > (select max(date) from payment_last ps);
 
 
 SELECT sum(amount) from payment p;
